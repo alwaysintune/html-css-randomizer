7b2fd7a273f3ac2dd731950202a75ac8a7bb4147
alwaysintune <domantas.slenys@gmail.com>
2022-05-14 15:43:16 +0300

    add shared constants for servers to use

---
+++ sharedConstants.js
@@ -0,0 +1,30 @@
+import { promises as fsp } from "fs";
+import { TimeValue } from "./utils/date";
+
+export const PROXY_PORT = 3001;
+export const PROXY_HOST = "localhost";
+export const PROXY_ADDRESS = `http://${PROXY_HOST}:${PROXY_PORT}`;
+
+export const SERVER_PORT = 8080;
+export const SERVER_HOST = "localhost";
+export const SERVER_ADDRESS = `http://${SERVER_HOST}:${SERVER_PORT}`;
+
+export const PUBLIC_KEY = await fsp.readFile("public_key.pem", "utf-8");
+export const PRIVATE_KEY = await fsp.readFile("private_key.pem", "utf-8");
+
+export const BUFFER_ENCODING = Buffer.isEncoding("base64url")
+  ? "base64url"
+  : "base64";
+
+export const HTML_ELEMENTS = new Set([
+  ...(await fsp.readFile("html_elements.csv", "utf-8"))
+    .toString()
+    .trim()
+    .split(","),
+  "no-js",
+]);
+
+export const WEBSITE_PATH = "./website/books.toscrape.com";
+
+export const FIRST_REQUEST_TTL_IN_SECONDS = TimeValue.getDaysInSeconds(1);
+export const FIRST_REQUEST_COOKIE = "first-request";
---
+++ website/path.js
@@ -0,0 +1,6 @@
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+
+export const WEBSITE_ROOT = path.dirname(__filename);
